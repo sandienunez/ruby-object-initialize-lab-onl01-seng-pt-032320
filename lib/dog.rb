fe6cc7afca22566f07a04443129060b5469cbba8
Sandie Nunez <snunez@vt.edu>
2020-04-13 21:33:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 require "pry"
 class Dog
+  #attr_accessor :name, :breed
   def initialize(name, breed = "Mutt")
     @name = name 
     @breed = breed
   end 
+  
+  
+  
   
   def breed 
    @breed 
@@ -13,5 +17,5 @@ class Dog
     @breed = breed_name
   end 
 end 
-timmy = Dog.new("Timmy", "pomeranian") 
-binding.pry
+#timmy = Dog.new("Timmy", "pomeranian") 
+#binding.pry
